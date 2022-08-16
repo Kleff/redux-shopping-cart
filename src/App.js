f754dda98fe796ca669f1021a628998b7373d1c3
Julian Scerra <julian.scerra@gmail.com>
2022-08-16 19:08:05 +0200

    starting with the asynchronous calls

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Auth from "./components/Auth";
+import { useDispatch, useSelector } from 'react-redux';
 import Layout from "./components/Layout";
+import Notification from './components/Notification';
+import { uiActions } from "./store/ui-slice";
 
 function App() {
+  const dispatch = useDispatch();
+  const cart = useSelector(state => state.cart);
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+
+  useEffect(() => {
+    const sendRequest = async () => {
+      // Sending state as Sending request
+      dispatch(uiActions.showNotification(){
+        open: true,
+        
+      })
+      try{
+        const res = await fetch('https://redux-http-5f762-default-rtdb.firebaseio.com/cartItems.json', {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        });
+        const data = await res.json();
+        // Send state as Request is successful
+      } catch(e){
+        // Send state as Error
+        console.error(e)
+      }
+      
+    }
+    
+    sendRequest();
+  }, [cart])
   return (
     <div className="App">
-      <Auth />
-      {/* <Layout /> */}
+      <Notification message="This is a dummy message" type="success"/>
+      {!isLoggedIn && <Auth />}
+      {isLoggedIn && <Layout />}
     </div>
   );
 }
---
+++ src/components/Notification.js
@@ -0,0 +1,11 @@
+import { Alert } from '@mui/material'; 
+
+const Notification = ({ type, message }) => {
+    return(
+        <div>
+            <Alert severity={type}>{message}</Alert>
+        </div>
+    )
+}
+
+export default Notification;
